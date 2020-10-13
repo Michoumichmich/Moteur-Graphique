9d586c76d7426afdc8ef66f9b7684cd642587e89
Novanef <trios.timothe@hotmail.fr>
2020-10-13 11:36:15 +0200

    implémentation vector

--- src/utils/Vector.cpp
+++ src/utils/Vector.cpp
@@ -1,1 +1,17 @@
 #include "Vector.h"
+
+Vector::Vector(double x, double y, double z) : Point3D(x, y, z) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+Vector::Vector(Point3D x, Point3D y) {
+    this->x = x.getX() - y.getX();
+    this->y = x.getY() - y.getY();
+    this->z = x.getZ() - y.getZ();
+}
+
+Vector::Vector() : Vector(0, 0, 0) {
+
+}
