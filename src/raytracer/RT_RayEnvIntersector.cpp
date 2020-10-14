ba8b6b5dcf25eabbbb47a5c71a984491b385e0ec
Michel Migdal <contact@mgdal.fr>
2020-10-14 13:03:07 +0200

    TODO

--- src/raytracer/RT_RayEnvIntersector.cpp
+++ src/raytracer/RT_RayEnvIntersector.cpp
@@ -14,9 +14,9 @@ bool RT_RayEnvIntersector::checkForSingleIntersection(Vector origin, Vector dir,
     Vector x0 = tessel.summmits[0];
     Vector x1 = tessel.summmits[1];
     Vector x2 = tessel.summmits[2];
-    double a0 = Vector::crossProduct(x1 - origin, x2 - origin)->dot(dir) * 0.5;
-    double a1 = Vector::crossProduct(x2 - origin, x0 - origin)->dot(dir) * 0.5;
-    double a2 = Vector::crossProduct(x0 - origin, x1 - origin)->dot(dir) * 0.5;
+    double a0 = Vector::crossProduct(x1 - origin, x2 - origin).dot(dir) * 0.5;
+    double a1 = Vector::crossProduct(x2 - origin, x0 - origin).dot(dir) * 0.5;
+    double a2 = Vector::crossProduct(x0 - origin, x1 - origin).dot(dir) * 0.5;
 
     if (likely(a0 < 0 && a1 < 0 && a2 < 0)) {
         return false;
