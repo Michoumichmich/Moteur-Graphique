12a6d416613157ee021c067250ce1e91303b8445
Lilian MALLARDEAU <lilian.mallardeau@ensiie.fr>
2020-10-15 18:15:30 +0200

    Added Cube object, with its tesselation function

---
+++ src/environment/RegularObjects/Cube.h
@@ -0,0 +1,20 @@
+//
+// Created by lilian on 15/10/2020.
+//
+
+#ifndef GRAPHICSENGINE_CUBE_H
+#define GRAPHICSENGINE_CUBE_H
+
+#include "../Object.h"
+
+class Cube : public Object {
+private:
+    double size;
+public:
+    explicit Cube(double size);
+
+    void Tesselate() override;
+};
+
+
+#endif //GRAPHICSENGINE_CUBE_H
