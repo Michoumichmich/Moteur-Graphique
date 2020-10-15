fffc9e3a33840f84bb2fc3c48e703b4ec9704ad0
Michel Migdal <contact@mgdal.fr>
2020-10-15 17:59:04 +0200

    Constructeurs pour la caméra

--- src/environment/Camera.cpp
+++ src/environment/Camera.cpp
@@ -1,10 +1,22 @@
 #include "Camera.h"
 
+#include <utility>
 
-Camera::Camera(std::string Name) {
-    //TODO
+
+Camera::Camera(Point3D origin, Point3D target) : target(target), origin(origin) {
 }
 
-Camera::Camera(Point3D origin, Point3D target) {
-    //TODO
+Camera::Camera(Point3D origin, Point3D target, std::string Name, enum camMode mode) : cameraName(std::move(Name)), mode(mode), origin(origin), target(target) {
 }
+
+Camera::Camera(Point3D origin, Point3D target, std::string name, enum camMode mode, double width, double height, Vector up) : cameraName(std::move(name)),
+                                                                                                                              mode(mode), origin(origin),
+                                                                                                                              target(target),
+                                                                                                                              viewportWidth(width),
+                                                                                                                              viewportHeight(height),
+                                                                                                                              viewUp(up) {
+
+}
+
+
+Camera::Camera() = default;
--- src/environment/Camera.h
+++ src/environment/Camera.h
@@ -12,29 +12,36 @@ class Camera {
 private:
 
 public:
-    double viewportWidth;
-    double viewportHeight;
+    double viewportWidth = 2;
+    double viewportHeight = 1;
 
     /**
      * Camera's view up vector.
      * TO obtain the real vector we project it on the view vector and substract that amount to the viewUP. Then we compute the right vector using a scalar
      * product and we norm everything
      */
-    Vector viewUp;
+    Vector viewUp = Vector(0, 1, 0);
 
     /**
      * Wether the rays are parallel or not
      */
-    enum camMode mode;
-    Point3D origin;
-    Point3D target;
-    std::string cameraName;
+    enum camMode mode = PERSPECTIVE;
+    Point3D origin = Point3D(0, 0, -2);
+    Point3D target = Point3D(0, 0, 1);
+    std::string cameraName = "Camera unnamed";
 
-    explicit Camera(std::string Name);
+
+    Camera();
 
     Camera(Point3D origin, Point3D target);
 
+    Camera(Point3D origin, Point3D target, std::string Name, enum camMode);
+
+    Camera(Point3D origin, Point3D target, std::string name, enum camMode mode, double width, double height, Vector up);
+
+
     void ModifyViewport(double width, double height);
+
 
     void PositionCamera(Point3D origin, Point3D target);
 };
