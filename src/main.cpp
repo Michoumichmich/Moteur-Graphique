fa38653fe37ad72e7ccdb8959a9e9a8d09f5c9b1
Vivien GAGLIANO <gagliano.vivien@gmail.com>
2020-10-15 10:35:34 +0200

    Set main.cpp to use CLI.

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,16 @@ int main(int argc, char **argv) {
      * Example using the CLI
      */
     auto *cli = new CommandLineInterface();
-    //cli->main_loop();
+    cli->main_loop();
     delete cli;
 
     /**
      * hardcoded example without the CLI
      */
-    auto *gr = new GraphicsEngine();
-    gr->createEnvironment("firstEnv");
-    RT_RayTracer *renderer = new RT_RayTracer(gr->getCurrentEnvironment(), gr->getPicManager());
-    gr->setRenderer(renderer);
+//    auto *gr = new GraphicsEngine();
+//    gr->createEnvironment("firstEnv");
+//    RT_RayTracer *renderer = new RT_RayTracer(gr->getCurrentEnvironment(), gr->getPicManager());
+//    gr->setRenderer(renderer);
 
 
 
@@ -23,20 +23,20 @@ int main(int argc, char **argv) {
     /**
      * Test intersection
      */
-    Vector dir = Vector(0, 11, 11);
-    Vector orig = Vector(0, 0, 1.1);
-    Environment *env = gr->getCurrentEnvironment();
-    Sphere *sph = new Sphere(1.0);
-    env->addObject(sph);
-    sph->Tesselate();
-
-    RT_RayEnvIntersector intersector = RT_RayEnvIntersector(env);
-    struct RT_RayIntersectionResult res = intersector.RT_RayFindIntersection(orig, dir);
-
-    OutputPictureManager pm = OutputPictureManager();
-    pm.savePicture();
-    delete gr;
-    delete renderer;
+//    Vector dir = Vector(0, 11, 11);
+//    Vector orig = Vector(0, 0, 1.1);
+//    Environment *env = gr->getCurrentEnvironment();
+//    Sphere *sph = new Sphere(1.0);
+//    env->addObject(sph);
+//    sph->Tesselate();
+//
+//    RT_RayEnvIntersector intersector = RT_RayEnvIntersector(env);
+//    struct RT_RayIntersectionResult res = intersector.RT_RayFindIntersection(orig, dir);
+//
+//    OutputPictureManager pm = OutputPictureManager();
+//    pm.savePicture();
+//    delete gr;
+//    delete renderer;
 
 
     /**
