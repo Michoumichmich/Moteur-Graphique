82056e33703771fd4d49a6f651491d347c7aa25d
Michel Migdal <contact@mgdal.fr>
2020-10-15 19:44:47 +0200

    Cast des rayons pour la vue en perspective

--- src/raytracer/RT_RayCaster.cpp
+++ src/raytracer/RT_RayCaster.cpp
@@ -1,6 +1,6 @@
 #include "RT_RayCaster.h"
 
-std::list<RT_Ray> RT_RayCaster::generateFirstRays(Camera *cam, OutputPictureManager *pic, unsigned int pixel_height_count, unsigned int pixel_width_count) {
+std::list<RT_Ray> RT_RayCaster::generateFirstRays(Camera *cam, unsigned int pixel_height_count, unsigned int pixel_width_count) {
     std::list<RT_Ray> Rays;
 
     if (cam->mode == ORTHOGRAPHIC) {
@@ -12,7 +12,18 @@ std::list<RT_Ray> RT_RayCaster::generateFirstRays(Camera *cam, OutputPictureMana
     } else if (cam->mode == PERSPECTIVE) {
         double pixel_width = cam->viewportWidth / pixel_width_count;
         double pixel_height = cam->viewportHeight / pixel_height_count;
-        //TODO
+        Vector view = Vector(cam->target, cam->origin);
+        Vector up_unit = (cam->viewUp - view.normalize() * (view.normalize().dot(cam->viewUp))).normalize();
+        // double dot = view.normalize().dot(cam->viewUp);
+        Vector right_unit = Vector::crossProduct(up_unit, view).normalize();
+        Point3D bottomLeft = Vector(cam->target) - up_unit * (cam->viewportHeight / 2.0) - right_unit * (cam->viewportWidth / 2.0);
+
+        for (unsigned x = 0; x < pixel_width_count; x++) {
+            for (unsigned y = 0; y < pixel_height_count; y++) {
+                Vector target = Vector(bottomLeft) + up_unit * (y / pixel_height) + right_unit * (x / pixel_width);
+                Rays.emplace_back(target, cam->origin, 1, true, x, y);
+            }
+        }
     }
 
     return Rays;
--- src/raytracer/RT_RayCaster.h
+++ src/raytracer/RT_RayCaster.h
@@ -11,7 +11,7 @@
 class RT_RayCaster {
 public:
     static std::list<RT_Ray>
-    generateFirstRays(Camera *cam, OutputPictureManager *pic, unsigned int pixel_height_count = DEFAULT_HEIGHT, unsigned int pixel_width_count = DEFAULT_WIDTH);
+    generateFirstRays(Camera *cam, unsigned int pixel_height_count = DEFAULT_HEIGHT, unsigned int pixel_width_count = DEFAULT_WIDTH);
 };
 
 #endif //GRAPHIC_ENGINE_RAYCASTER_H
