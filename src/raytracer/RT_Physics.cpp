ae2b873eb7bd5787e5575d490f57881f566bfe6c
Vivien GAGLIANO <gagliano.vivien@gmail.com>
2020-11-11 19:32:47 +0100

    Updated computePhongIllumination fonction so that it also works for non-primary rays.

--- src/raytracer/RT_Physics.cpp
+++ src/raytracer/RT_Physics.cpp
@@ -1,6 +1,6 @@
 #include "RT_Physics.h"
 
-Vector RT_Physics::computePhongIllumination(Vector p, Vector n, Vector ambientIntensity, std::list<Light> lights, ApparenceProperties properties) {
+Vector RT_Physics::computePhongIllumination(Vector p, Vector n, Vector incomingRay, Vector ambientIntensity, std::list<Light> lights, ApparenceProperties properties) {
     /**
      * Ambient component
      */
@@ -9,12 +9,12 @@ Vector RT_Physics::computePhongIllumination(Vector p, Vector n, Vector ambientIn
     /**
      * Diffuse and specular components
      */
-    Vector cameraDirection = (p - cameraPosition).normalize();
+    incomingRay.normalize();
     std::list<Light>::iterator it;
     for (it = lights.begin(); it != lights.end(); it++) {
         Vector lightDirection = (it->position - p).normalize();
         intensity+= properties.materialDiffuseIntensity * it->intensity * n.dot(lightDirection); /**< Diffuse term */
-        double specular = n.dot((lightDirection + cameraDirection).normalize());
+        double specular = n.dot((lightDirection + incomingRay).normalize());
         intensity+= properties.materialSpecularIntensity * it->intensity * specular; /**< Specular term, computed with Blinn's formula */
     }
 
--- src/raytracer/RT_Physics.h
+++ src/raytracer/RT_Physics.h
@@ -6,17 +6,17 @@
 
 class RT_Physics {
     public:
-        static Vector cameraPosition;
         /**
          * Computes light intensity for given point in space
          * @param p world coordinates for the position
          * @param n normal vector of the surface where the point is
+         * @param incomingRay vector with the direction with wich the ray hits the surface on point p
          * @param ambientIntensity vector with light intensity of ambient space in RGB components
          * @param lights list of light sources present in the scene
          * @return vector with RGB components of the light intensity in given space position
          * @remark formula explained here http://rodolphe-vaillant.fr/?e=85
          */
-        static Vector computePhongIllumination(Vector p, Vector n, Vector ambientIntensity, std::list<Light> lights, ApparenceProperties properties);
+        static Vector computePhongIllumination(Vector p, Vector n, Vector incomingRay, Vector ambientIntensity, std::list<Light> lights, ApparenceProperties properties);
 };
 
 
