a04369696538de81e9be23506a49f814cb2bbe47
Vivien GAGLIANO <gagliano.vivien@gmail.com>
2020-11-11 18:41:40 +0100

    Added Light.h to environment include.

--- src/include/environment.h
+++ src/include/environment.h
@@ -5,6 +5,7 @@
 #include "../environment/Object.h"
 #include "../environment/Tessel.h"
 #include "../environment/Camera.h"
+#include "../environment/Light.h"
 #include "../environment/ApparenceProperties.h"
 #include "../environment/MappedTexture.h"
 #include "../environment/RegularObjects/Sphere.h"
