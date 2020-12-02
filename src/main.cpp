106d479008d9f625351f0529512a6333b41bf2b2
Vivien GAGLIANO <gagliano.vivien@gmail.com>
2020-12-03 00:46:57 +0100

    Forgotten in previous commits.

--- src/main.cpp
+++ src/main.cpp
@@ -23,38 +23,38 @@ int main(int argc, char **argv) {
      * Example using the CLI
      */
     auto cli = std::make_unique<CommandLineInterface>();
-    //   cli->main_loop();
+    cli->main_loop();
 
     /**
      * hardcoded example without the CLI
      */
-    std::unique_ptr<GraphicsEngine> gr = std::make_unique<GraphicsEngine>();
-    gr->createEnvironment("firstEnv");
-
-    Cube *cube_center = new Cube(1);
-    Cube *cube_left = new Cube(1);
-    Cube *cube_right = new Cube(1);
-    cube_right->setTransformation({0.5, 0, 45, 0, Vector(-1.2, 0.25, -0.25)});
-    cube_left->setTransformation({0.5, 0, 0, 45, Vector(1.2, 0.25, -0.25)})->setColor(Color(138, 26, 70));
-    gr->addObjInEnv(cube_left);
-    gr->addObjInEnv(cube_center);
-    gr->addObjInEnv(cube_right->setColor(Color(0, 85, 108)));
-    gr->addObjInEnv(new Sphere(1 / 1.42));
-    gr->currEnv()->setResolution(40);
-    gr->setRenderer(new RT_RayTracer());
-
-
-    auto *cam = new Camera("Face");
-    cam->setMode(PERSPECTIVE);
-    cam->setResolution(2001, 1001);
-    cam->setViewDimensions(4, 2);
-    cam->setDirection(Point3D(-1.01, 2.01, -2.01), Point3D(0, 0, 0));
-    gr->currEnv()->addCamera(cam);
-    gr->currEnv()->switchCamera("Face");
-    gr->launchRender("test_face.bmp");
-
-    gr->currEnv()->switchCamera("Default");
-    gr->launchRender("test_default.bmp");
+//    std::unique_ptr<GraphicsEngine> gr = std::make_unique<GraphicsEngine>();
+//    gr->createEnvironment("firstEnv");
+//
+//    Cube *cube_center = new Cube(1);
+//    Cube *cube_left = new Cube(1);
+//    Cube *cube_right = new Cube(1);
+//    cube_right->setTransformation({0.5, 0, 45, 0, Vector(-1.2, 0.25, -0.25)});
+//    cube_left->setTransformation({0.5, 0, 0, 45, Vector(1.2, 0.25, -0.25)})->setColor(Color(138, 26, 70));
+//    gr->addObjInEnv(cube_left);
+//    gr->addObjInEnv(cube_center);
+//    gr->addObjInEnv(cube_right->setColor(Color(0, 85, 108)));
+//    gr->addObjInEnv(new Sphere(1 / 1.42));
+//    gr->currEnv()->setResolution(40);
+//    gr->setRenderer(new RT_RayTracer());
+//
+//
+//    auto *cam = new Camera("Face");
+//    cam->setMode(PERSPECTIVE);
+//    cam->setResolution(2001, 1001);
+//    cam->setViewDimensions(4, 2);
+//    cam->setDirection(Point3D(-1.01, 2.01, -2.01), Point3D(0, 0, 0));
+//    gr->currEnv()->addCamera(cam);
+//    gr->currEnv()->switchCamera("Face");
+//    gr->launchRender("test_face.bmp");
+//
+//    gr->currEnv()->switchCamera("Default");
+//    gr->launchRender("test_default.bmp");
 
 //#ifdef BENCHMARK
 //    int lap_count = 1;
