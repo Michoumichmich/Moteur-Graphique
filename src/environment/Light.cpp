39fc6e37db5a85fad4f5f65418b6779fdef87d2e
Lilian MALLARDEAU <lilian.mallardeau@ensiie.fr>
2020-12-03 04:39:07 +0100

    Added << operator overload for Light

--- src/environment/Light.cpp
+++ src/environment/Light.cpp
@@ -1,3 +1,8 @@
 #include "Light.h"
 
 Light::Light(Vector p, double intensity) : position(p), intensity(intensity) {};
+
+std::ostream& operator<< (std::ostream &out, const Light &light) {
+    out << "Light(" << light.position << ", " << light.intensity << ")";
+    return out;
+}
