943d10bcef61812752ac98cabaa640cc6684a59e
Vivien GAGLIANO <gagliano.vivien@gmail.com>
2020-12-17 02:27:09 +0100

    Added generate_pyramid function

--- src/code_bits.hpp
+++ src/code_bits.hpp
@@ -27,6 +27,29 @@ static inline std::shared_ptr<Cube> cube_generator()
     return cube;
 }
 
+static inline std::shared_ptr<Pyramid> pyramid_generator()
+{
+
+    double size = ((double) rand()/(double) RAND_MAX)*0.05+0.09;
+
+    double pyramid_light = (double) rand() / (double) RAND_MAX;
+
+    auto pyramid = std::make_shared<Pyramid>(size);
+    struct transformations transfo;
+    transfo.rotX = rand() % 90;
+    transfo.rotY = rand() % 45;
+    transfo.rotZ = rand() % 45;
+    double rand1 = (double) rand() / (double) RAND_MAX;
+    double rand2 = (double) rand() / (double) RAND_MAX;
+    double rand3 = (double) rand() / (double) RAND_MAX;
+    transfo.pt = Vector(-4 + rand1 * 8, -4 + rand2 * 8, -4 + rand3 * 8);
+    pyramid->setTransformation(transfo);
+    pyramid->setColor(Color(rand() % 256, rand() % 256, rand() % 256));
+    pyramid->setLightIntensity(.01);
+    pyramid->setReflexivity(0.8);
+    return pyramid;
+}
+
 static void inline default_test()
 {
     /**
@@ -35,12 +58,12 @@ static void inline default_test()
     std::unique_ptr<GraphicsEngine> gr = std::make_unique<GraphicsEngine>();
     gr->createEnvironment("firstEnv");
 
-    std::shared_ptr<Cube> cube_center = std::make_shared<Cube>(1);
-    cube_center->setColor(Color(200, 200, 200));
-    cube_center->setLightIntensity(0.99);
-    cube_center->setReflexivity(0);
-    //  cube_center->setTransformation({0.5, 0, 20, 45, Vector(-1.2, 0.25, -0.25)})->setColor(Color(138, 26, 70));
-    gr->addObjInEnv(cube_center);
+//    std::shared_ptr<Cube> cube_center = std::make_shared<Cube>(1);
+//    cube_center->setColor(Color(200, 200, 200));
+//    cube_center->setLightIntensity(0.99);
+//    cube_center->setReflexivity(0);
+//    //  cube_center->setTransformation({0.5, 0, 20, 45, Vector(-1.2, 0.25, -0.25)})->setColor(Color(138, 26, 70));
+//    gr->addObjInEnv(cube_center);
 
     std::shared_ptr<Cube> cube_left = std::make_shared<Cube>(1);
     cube_left->setReflexivity(0.51);
@@ -56,16 +79,34 @@ static void inline default_test()
 
     std::shared_ptr<Sphere> sphere = std::make_shared<Sphere>(1 / 1.42);
     sphere->setLightIntensity(0);
-    sphere->setColor(Color(0.2, 0.0, 0.5));
+    sphere->setColor(Color(1.0, 1.0, 1.0));
     sphere->setReflexivity(0.8);
     gr->addObjInEnv(sphere);
 
-    for (int i = 0; i < 450; i++) gr->addObjInEnv(cube_generator());
+    std::shared_ptr<Pyramid> pyramid_center = std::make_shared<Pyramid>(1);
+    pyramid_center->setColor(Color(125, 0, 200));
+    pyramid_center->setLightIntensity(.1);
+    pyramid_center->setReflexivity(1);
+    pyramid_center->setTransformation({2, 15, -5, 30, Vector(0, 0, 0)});
+    gr->addObjInEnv(pyramid_center);
 
-    auto plane = std::make_shared<Plane>(30, 30);
-    plane->setColor(Color(0));
-    plane->setTransformation({1, -25, 0, 0, Vector(0, 0, -1)});
-    gr->addObjInEnv(plane);
+    for (int i = 0; i < 300; i++) gr->addObjInEnv(cube_generator());
+    for (int i = 0; i < 300; ++i) gr->addObjInEnv(pyramid_generator());
+
+//    auto plane = std::make_shared<Plane>(30, 30);
+//    plane->setColor(Color(0));
+//    plane->setTransformation({1, 0, 0, 0, Vector(0, 0, -1)});
+//    gr->addObjInEnv(plane);
+
+    auto planeLeft = std::make_shared<Plane>(5, 5);
+    planeLeft->setColor(Color(1));
+    planeLeft->setTransformation({1, 0, 90, 0, Vector(-2.5, 0, 0)});
+    gr->addObjInEnv(planeLeft);
+
+    auto planeRight = std::make_shared<Plane>(5, 5);
+    planeRight->setColor(Color(0));
+    planeRight->setTransformation({1, 0, 90, 0, Vector(2.5, 0, 0)});
+    gr->addObjInEnv(planeRight);
 
     auto plane_top = std::make_shared<Plane>(30, 30);
     plane_top->setColor(Color(1));
@@ -80,12 +121,12 @@ static void inline default_test()
 
     auto raytracer = new RT_RayTracer();
     raytracer->setMode(RT_RayRenderMode::RT_STANDARD);
-    raytracer->setMaxBounces(4);
+    raytracer->setMaxBounces(6);
     gr->setRenderer(raytracer);
 
     std::shared_ptr<Camera> cam = std::make_shared<Camera>("Face");
     cam->setMode(PERSPECTIVE);
-    cam->setResolution(501, 251);
+    cam->setResolution(6001, 3001);
     cam->setViewDimensions(6, 3);
     cam->setDirection(Point3D(0.01, 5.01, -2.01), Point3D(0, 0, 0));
 
