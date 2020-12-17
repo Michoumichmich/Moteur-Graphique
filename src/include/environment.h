98330958d0a11cde1c6e9919d0e22b2ea0d1d43f
Michel Migdal <contact@mgdal.fr>
2020-12-17 01:05:30 +0100

    New 2d plane object

--- src/include/environment.h
+++ src/include/environment.h
@@ -10,5 +10,6 @@
 #include "../environment/MappedTexture.h"
 #include "../environment/RegularObjects/Sphere.h"
 #include "../environment/RegularObjects/Cube.h"
+#include "../environment/RegularObjects/Plane.h"
 
 #endif //GRAPHIC_ENGINE_ENVIRONMENT_H
