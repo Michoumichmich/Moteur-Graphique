7887bbbdce3d9cd2b1c460b7eb6b0853b8425c40
Michel Migdal <contact@mgdal.fr>
2020-12-17 20:32:37 +0100

    New depthmap exclusively in BW

--- src/code_bits.hpp
+++ src/code_bits.hpp
@@ -4,10 +4,9 @@
 #include <graphicsEngine.h>
 #include <memory>
 
-static inline std::shared_ptr<Cube> cube_generator()
-{
+static inline std::shared_ptr<Cube> cube_generator() {
 
-    double size = ((double) rand()/(double) RAND_MAX)*0.05+0.09;
+    double size = ((double) rand() / (double) RAND_MAX) * 0.05 + 0.09;
 
     double cube_light = (double) rand() / (double) RAND_MAX;
     auto cube = std::make_shared<Cube>(size);
@@ -20,8 +19,8 @@ static inline std::shared_ptr<Cube> cube_generator()
     double rand3 = (double) rand() / (double) RAND_MAX;
     transfo.pt = Vector(-4 + rand1 * 8, -4 + rand2 * 8, -4 + rand3 * 8);
     cube->setTransformation(transfo);
-    cube->setColor(Color(rand()%256, rand()%256, rand()%256));
-    cube->setLightIntensity(cube_light/2.+0.25);
+    cube->setColor(Color(rand() % 256, rand() % 256, rand() % 256));
+    cube->setLightIntensity(cube_light / 2. + 0.25);
     cube->setReflexivity(1);
     return cube;
 }
@@ -48,21 +47,20 @@ static inline std::shared_ptr<Pyramid> pyramid_generator() {
     return pyramid;
 }
 
-static void inline default_test()
-{
-    /**
-     * hardcoded example without the CLI
-     */
+
+/**
+ * Premier exemple
+ */
+static void inline default_test() {
     std::unique_ptr<GraphicsEngine> gr = std::make_unique<GraphicsEngine>();
     gr->createEnvironment("firstEnv");
-
-//    std::shared_ptr<Cube> cube_center = std::make_shared<Cube>(1);
-//    cube_center->setColor(Color(200, 200, 200));
-//    cube_center->setLightIntensity(0.99);
-//    cube_center->setReflexivity(0);
-//    //  cube_center->setTransformation({0.5, 0, 20, 45, Vector(-1.2, 0.25, -0.25)})->setColor(Color(138, 26, 70));
-//    gr->addObjInEnv(cube_center);
-
+/*
+    std::shared_ptr<UglySphere> ew = std::make_shared<UglySphere>(1, 100, 500);
+    ew->setLightIntensity(0.05);
+    ew->setColor(Color(1));
+    ew->setReflexivity(1);
+    gr->addObjInEnv(ew);
+*/
     std::shared_ptr<Cube> cube_left = std::make_shared<Cube>(1);
     cube_left->setReflexivity(0.51);
     cube_left->setLightIntensity(0.5);
@@ -90,11 +88,6 @@ static void inline default_test()
 
     for (int i = 0; i < 400; i++) gr->addObjInEnv(cube_generator());
     for (int i = 0; i < 400; ++i) gr->addObjInEnv(pyramid_generator());
-
-//    auto plane = std::make_shared<Plane>(30, 30);
-//    plane->setColor(Color(0));
-//    plane->setTransformation({1, 0, 0, 0, Vector(0, 0, -1)});
-//    gr->addObjInEnv(plane);
 
     auto planeLeft = std::make_shared<Plane>(5, 5);
     planeLeft->setColor(Color(1));
@@ -128,7 +121,7 @@ static void inline default_test()
     std::shared_ptr<Camera> cam = std::make_shared<Camera>("Face");
     cam->setMode(PERSPECTIVE);
     cam->setResolution(4001, 2001);
-    cam->setDownSamplingFactor(2);
+    cam->setDownSamplingFactor(1);
     cam->setViewDimensions(6, 3);
     cam->setDirection(Point3D(0.01, 5.01, -2.01), Point3D(0, 0, 0));
 
@@ -142,42 +135,20 @@ static void inline default_test()
     gr->currEnv()->print_objects(std::cout);
     gr->launchRender("test_face.bmp");
     gr->currEnv()->switchCamera("Default");
-
-    //   gr->launchRender("test_default.bmp");
-
-//#ifdef BENCHMARK
-//    int lap_count = 1;
-//    double avg = 0;
-//    for (int i = 0; i < lap_count; i++) {
-//        auto start = std::chrono::high_resolution_clock::now();
-//#endif
-//        gr->launchRender("test.bmp");
-//#ifdef BENCHMARK
-//        auto end = std::chrono::high_resolution_clock::now();
-//        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-//        std::cout << "time " << duration.count() / 1000000.0 << "s" << std::endl;
-//        avg += duration.count();
-//    }
-//    std::cout << "avg time " << avg / (1000000.0 * lap_count) << "s" << std::endl;
-//#endif
-//
-//    std::stringstream ss;
-//    gr->serialize(ss);
-//    std::cout << ss.str() << std::endl;
 }
 
 static void inline default_test2() {
-    /**
-     * hardcoded example without the CLI
-     */
     std::unique_ptr<GraphicsEngine> gr = std::make_unique<GraphicsEngine>();
     gr->createEnvironment("firstEnv");
 
-    /* std::shared_ptr<Cube> cube_center = std::make_shared<Cube>(1);
-     cube_center->setColor(Color(200, 200, 200));
-     cube_center->setLightIntensity(0.99);
-     cube_center->setReflexivity(0);
-     gr->addObjInEnv(cube_center); */
+    for (int i = 0; i < 400; i++) gr->addObjInEnv(cube_generator());
+    for (int i = 0; i < 400; ++i) gr->addObjInEnv(pyramid_generator());
+
+    std::shared_ptr<Cube> cube_center = std::make_shared<Cube>(1);
+    cube_center->setColor(Color(200, 200, 200));
+    cube_center->setLightIntensity(0.99);
+    cube_center->setReflexivity(0);
+    gr->addObjInEnv(cube_center);
 
     std::shared_ptr<Cube> cube_left = std::make_shared<Cube>(1);
     cube_left->setReflexivity(0.51);
@@ -191,22 +162,18 @@ static void inline default_test2() {
     cube_right->setTransformation({0.5, 0, 20, 45, Vector(1.2, 0.25, -0.25)});
     gr->addObjInEnv(cube_right);
 
-    /*  std::shared_ptr<Sphere> sphere = std::make_shared<Sphere>(1/1.42);
-      sphere->setLightIntensity(0);
-      sphere->setColor(Color(0.2, 0.0, 0.5));
-      sphere->setReflexivity(0.8);
-      gr->addObjInEnv(sphere); */
+    std::shared_ptr<Sphere> sphere = std::make_shared<Sphere>(1 / 1.42);
+    sphere->setLightIntensity(0);
+    sphere->setColor(Color(0.2, 0.0, 0.5));
+    sphere->setReflexivity(0.8);
+    gr->addObjInEnv(sphere);
 
-
-    std::shared_ptr<UglySphere> ew = std::make_shared<UglySphere>(2, 100, 100);
-    ew->setLightIntensity(0);
-    ew->setColor(Color(0.2, 0.0, 0.5));
+/*  std::shared_ptr<UglySphere> ew = std::make_shared<UglySphere>(1, 100, 1000);
+    ew->setLightIntensity(0.05);
+    ew->setColor(Color(1));
     ew->setReflexivity(1);
     gr->addObjInEnv(ew);
-
-    for (int i = 0; i < 400; i++) gr->addObjInEnv(cube_generator());
-    for (int i = 0; i < 400; ++i) gr->addObjInEnv(pyramid_generator());
-
+*/
     auto plane = std::make_shared<Plane>(50, 50);
     plane->setColor(Color(0.03));
     plane->setLightIntensity(0.9);
@@ -221,13 +188,13 @@ static void inline default_test2() {
 
     auto raytracer = new RT_RayTracer();
     raytracer->setMode(RT_RayRenderMode::RT_STANDARD);
-    raytracer->setMaxBounces(3);
+    raytracer->setMaxBounces(6);
     gr->setRenderer(raytracer);
 
     std::shared_ptr<Camera> cam = std::make_shared<Camera>("Face");
     cam->setMode(PERSPECTIVE);
-    cam->setResolution(2001, 1001);
-    cam->setDownSamplingFactor(1);
+    cam->setResolution(4001, 2001);
+    cam->setDownSamplingFactor(2);
     cam->setViewDimensions(10, 5);
     cam->setDirection(Point3D(0.01, 5.01, -2.01), Point3D(0, 0, 0));
 
