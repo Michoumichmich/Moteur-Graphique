f7b67cb975fdfd54e8e086a47bc278b170bd30d9
Michel Migdal <contact@mgdal.fr>
2020-12-17 03:13:18 +0100

    New 2d plane object

--- src/code_bits.hpp
+++ src/code_bits.hpp
@@ -85,7 +85,8 @@ static void inline default_test()
 
     std::shared_ptr<Camera> cam = std::make_shared<Camera>("Face");
     cam->setMode(PERSPECTIVE);
-    cam->setResolution(5001, 2501);
+    cam->setResolution(5000, 2500);
+    cam->setDownSamplingFactor(1);
     cam->setViewDimensions(6, 3);
     cam->setDirection(Point3D(0.01, 5.01, -2.01), Point3D(0, 0, 0));
 
